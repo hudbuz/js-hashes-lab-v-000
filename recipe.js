d3927d1272d4dfa752ff252a0a78feeeab1fef93
hudbuz <hzb2@cornell.edu>
2016-07-15 11:46:02 -0400

    Done.

--- recipe.js
+++ recipe.js
@@ -1,1 +1,31 @@
 'use strict';
+
+
+function addIngredient(object, key, value){
+
+  object[key] = value;
+  return object;
+
+}
+
+function removeIngredient(object, key, value){
+
+  delete object[key];
+  return object;
+
+}
+
+
+function updateIngredient(object, key, value){
+
+  object[key] = value;
+  return object;
+
+}
+
+function readRecipe(object) {
+
+  for (var key in object) {
+    console.log("this recipe calls for " + object[key] + " of " + key);
+  }
+}
